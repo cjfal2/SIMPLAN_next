3fc0ed3a20fc8b33b217960126cb7827d34e670a
cjfal2 <cjfal2@kakao.com>
2024-01-26 21:33:02 +0900

    cors 6

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/:path*",
+        source: "https://gittgi.site/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
