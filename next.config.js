9edfc4924cb9da1ed0d4c97b100cbfcac58d252b
cjfal2 <cjfal2@kakao.com>
2024-01-26 21:39:42 +0900

    cors 9

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ module.exports = {
     return [
       {
         source: '/api/:path*',
-        destination: `https://gittgi.site/:path*`,
+        destination: `/:path*`,
       },
     ];
   },
