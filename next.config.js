e414f08d8e2729c81b32ceae2363ab719b438a48
cjfal2 <cjfal2@kakao.com>
2024-01-27 22:32:13 +0900

    cors 2일차 6

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
         // matching all API routes
         source: "/:path*",
         headers: [
-          { key: "Access-Control-Expose-Headers:", value: "Authorization" },
+          { key: "Access-Control-Expose-Headers", value: "Authorization" },
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
