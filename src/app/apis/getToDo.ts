9c5d06e20f9be66e1fc4105eae318ced793443ed
cjfal2 <cjfal2@kakao.com>
2024-01-27 22:56:52 +0900

    api + rewrites + url 지우기

--- src/app/apis/getToDo.ts
+++ src/app/apis/getToDo.ts
@@ -23,7 +23,7 @@ export async function getTodayToDo() {
   const date = `${year}-${m}-${d}`;
 
   let todayToDo: DailyToDoType = new Array();
-  const apiUrl = "https://gittgi.site/plan";
+  const apiUrl = "/plan";
 
   if (accessToken) {
     try {
