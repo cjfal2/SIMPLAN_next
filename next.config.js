cb25f9e039eede46f5f805cd734911dfed45c563
cjfal2 <cjfal2@kakao.com>
2024-01-27 23:48:28 +0900

    404 떠서 test

--- next.config.js
+++ next.config.js
@@ -2,22 +2,22 @@
 
 module.exports = {
   reactStrictMode: false,
-  // async headers() {
-  //   return [
-  //     {
-  //       // matching all API routes
-  //       source: "/api/:path*",
-  //       headers: [
-  //         // { key: "Access-Control-Expose-Headers", value: "Authorization" },
-  //         { key: "Access-Control-Allow-Credentials", value: "true" },
-  //         // { key: "Access-Control-Allow-Origin", value: "https://simplan-next.vercel.app, http://localhost:3000, https://gittgi.site" },
-  //         { key: "Access-Control-Allow-Origin", value: "*" },
-  //         { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-  //         { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
-  //       ]
-  //     }
-  //   ]
-  // },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          // { key: "Access-Control-Expose-Headers", value: "Authorization" },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          // { key: "Access-Control-Allow-Origin", value: "https://simplan-next.vercel.app, http://localhost:3000, https://gittgi.site" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
+        ]
+      }
+    ]
+  },
   async rewrites() {
     return [
       {
