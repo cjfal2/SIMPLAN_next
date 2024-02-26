eaa3c045afa94d36b5c05a3d244d7232259ed7ce
cjfal2 <cjfal2@kakao.com>
2024-02-26 15:58:10 +0900

    쿠키는 use client를 쓸 수 없음

--- src/app/check/page.tsx
+++ src/app/check/page.tsx
@@ -1,18 +1,15 @@
-'use client'
-
 import Link from "next/link";
 import React, { useEffect } from "react";
 import { cookies } from 'next/headers'
 
 
 export default function CheckPage() {
-  useEffect(() => {
+
     const cookieStore = cookies()
     const theme = cookieStore.get('RefreshToken')
     // 쿠키에 저장된 값 콘솔에 출력
     console.log("쿠키에 저장된 값:", theme);
-  }, []);
-
+  
   return (
     <div className="w-full justify-center items-center flex">
       <Link href={"/"}>처음으로처음으로처음으로처음으로처음으로</Link>
