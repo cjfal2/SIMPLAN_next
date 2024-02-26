91dd2bd2cffc0787d4fc6d03bc977a6d18444a94
cjfal2 <cjfal2@kakao.com>
2024-02-26 16:20:12 +0900

    꾸끼 4

--- src/app/check/page.tsx
+++ src/app/check/page.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import React, { useEffect } from "react";
 import { cookies } from 'next/headers'
-
+import { NextResponse } from 'next/server'
 
 export default function CheckPage() {
-
+    let response = NextResponse.next()
+    response.cookies.set('RefreshToken', 'false')
     const cookieStore = cookies()
     const theme = cookieStore.get('RefreshToken')
     // 쿠키에 저장된 값 콘솔에 출력
