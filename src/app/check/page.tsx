46bcd00956078e487f9ca7108aa1c0dfde6d92ef
cjfal2 <cjfal2@kakao.com>
2024-02-26 15:49:11 +0900

    쿠키해보기2

--- src/app/check/page.tsx
+++ src/app/check/page.tsx
@@ -2,29 +2,15 @@
 
 import Link from "next/link";
 import React, { useEffect } from "react";
-import axios from "axios";
+import { cookies } from 'next/headers'
+
 
 export default function CheckPage() {
   useEffect(() => {
-    // 쿠키에서 값을 가져오는 함수
-    const getCookie = (name: string) => {
-      const value: string = `; ${document.cookie}`;
-      const parts: string[] = value.split(`; ${name}=`);
-
-      // 쿠키를 찾지 못한 경우
-      if (parts.length === 2) {
-        return parts.pop()?.split(";").shift();
-      } else {
-        console.error("쿠키를 찾을 수 없습니다.");
-        return undefined;
-      }
-    };
-
-    // 쿠키에서 저장된 값 가져오기
-    const myCookieValue = getCookie("RefreshToken"); // 'myCookieName'은 실제 쿠키의 이름으로 대체되어야 합니다.
-
+    const cookieStore = cookies()
+    const theme = cookieStore.get('RefreshToken')
     // 쿠키에 저장된 값 콘솔에 출력
-    console.log("쿠키에 저장된 값:", myCookieValue);
+    console.log("쿠키에 저장된 값:", theme);
   }, []);
 
   return (
