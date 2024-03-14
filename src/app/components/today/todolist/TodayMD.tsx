1c1010d3d9ae687130cbfec11ffee89340025a94
cjfal2 <cjfal2@kakao.com>
2024-03-14 15:12:48 +0900

    메인 날짜 수정

--- src/app/components/today/todolist/TodayMD.tsx
+++ src/app/components/today/todolist/TodayMD.tsx
@@ -1,12 +1,10 @@
+import { getTodayDate } from "@/app/apis/getTodayDate";
 import React from "react";
 const daysOfWeek: string[] = ["일", "월", "화", "수", "목", "금", "토"];
 
 export default function TodayMD() {
-  const today = new Date();
-  const month = today.getMonth();
-  const day = today.getDate();
-  const date = today.getDay();
+  let [yearToday, monthToday, dayToday, dayOfWeekToday]: number[] = getTodayDate();
   return (
-    <div className="text-2xl font-semibold">{`${month + 1}월 ${day}일 ${daysOfWeek[date]}요일`}</div>
+    <div className="text-2xl font-semibold">{`${monthToday}월 ${dayToday}일 ${daysOfWeek[dayOfWeekToday]}요일`}</div>
   );
 }
